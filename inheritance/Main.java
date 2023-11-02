b4d194c3beb4ac28f4fae9a08901f38fe45c7813
Libria Putri <145516973+libriaputri@users.noreply.github.com>
2023-11-02 19:39:38 +0700

    Update Main.java

--- inheritance/Main.java
+++ inheritance/Main.java
@@ -12,15 +12,27 @@ public class Main {
     public static void main(String[] args) {
         Enemy monster = new Enemy();
         
-        Zombie zumbi = new Zombie();
+       Zombie zumbi = new Zombie();
+        zumbi.name = "Nunu";
+        zumbi.hp= 80;
+        zumbi.attackpoin=15;
+        System.out.println(zumbi.name +" Healt Point = "+zumbi.hp + " Attack poin = "+zumbi.attackpoin);
         zumbi.attack();
         zumbi.walk();
         
         Pocong hantupocong = new Pocong();
+        hantupocong.name = "Ocong";
+        hantupocong.hp = 70;
+        hantupocong.attackpoin = 80;
+        System.out.println(hantupocong.name + " Health point = " + hantupocong.hp + " Attack Point = " + hantupocong.attackpoin);
         hantupocong.attack();
         hantupocong.jump();
         
         Burung garuda = new Burung();
+        garuda.name = "Buyung";
+        garuda.hp = 85;
+        garuda.attackpoin = 90;
+        System.out.println(hantupocong.name + " Health point = " + hantupocong.hp + " Attack Point = " + hantupocong.attackpoin);
         garuda.attack();
         garuda.fly();
         garuda.jump();
