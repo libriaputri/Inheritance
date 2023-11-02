f868bda4b55d36254843f0398c929a2991118ee9
Libria Putri <145516973+libriaputri@users.noreply.github.com>
2023-11-02 19:43:43 +0700

    Update Lingkaran.java

--- inheritanceBangundatar/Lingkaran.java
+++ inheritanceBangundatar/Lingkaran.java
@@ -10,4 +10,17 @@ package inheritanceBangundatar;
  */
 public class Lingkaran extends BangunDatar {
     float r;
+        
+     @Override
+   float luas(){
+       float luas = (float) (Math.PI * r*r);
+       System.out.println("Luas Lingkaran = " + luas);
+       return luas;
+   }
+   @Override
+   float keliling(){
+       float keliling = (float)  (2 * Math.PI *r);
+       System.out.println("Kelliling Persegi = " + keliling);
+       return keliling;
+   }
 }
